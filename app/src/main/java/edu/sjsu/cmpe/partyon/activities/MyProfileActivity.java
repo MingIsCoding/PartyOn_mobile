ee1dc88409dc31098bcb196a5c4ff6687d6771a1
navdeep2237 <navdeep2237@gmail.com>
2016-11-28 15:24:11 -0800

    Posts images view

---
+++ app/src/main/java/edu/sjsu/cmpe/partyon/activities/MyProfileActivity.java
@@ -0,0 +1,15 @@
+package edu.sjsu.cmpe.partyon.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.sjsu.cmpe.partyon.R;
+
+public class MyProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}

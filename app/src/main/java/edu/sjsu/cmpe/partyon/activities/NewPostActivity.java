84362ba3b92fc0a3703db471ccf0a25ae8612715
navdeep2237 <navdeep2237@gmail.com>
2016-11-28 14:46:01 -0800

    Posts images view

--- app/src/main/java/edu/sjsu/cmpe/partyon/activities/NewPostActivity.java
+++ app/src/main/java/edu/sjsu/cmpe/partyon/activities/NewPostActivity.java
@@ -1,5 +1,9 @@
 package edu.sjsu.cmpe.partyon.activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,10 +12,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
 
 import edu.sjsu.cmpe.partyon.R;
 import edu.sjsu.cmpe.partyon.entities.Post;
@@ -20,29 +30,50 @@ import edu.sjsu.cmpe.partyon.entities.User;
 public class NewPostActivity extends CloseableActivity {
     private String mPostTxt;
     private EditText mPostTxtView;
+    private ImageView mImageView;
+    private Post post;
+    private Bitmap bitmap;
+    private ParseFile imageFile;
+    private ProgressBar spinner;
+    private static String TAG = "POST THINGY";
 
     @Override
     public void onCreate(Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_new_post);
         //setupToolBar();
+
         initView();
     }
 
     private void initView(){
         mPostTxtView = (EditText)findViewById(R.id.post_content_txt);
+        mImageView = (ImageView) findViewById(R.id.post_img_view);
+        spinner = (ProgressBar) findViewById(R.id.progress_bar_post);
+        spinner.setVisibility(View.GONE);
+
+        Toast.makeText(this, "new Post View", Toast.LENGTH_LONG).show();
+        byte[] passedByte = getIntent().getByteArrayExtra("thumbImage");
+        bitmap = BitmapFactory.decodeByteArray(passedByte, 0, passedByte.length);
+        mImageView.setImageBitmap(bitmap);
+
     }
     private void savePost(){
-        Post post = new Post();
+        post = new Post();
         post.setTextContent(mPostTxtView.getText().toString());
         post.setAuthor((User)User.getCurrentUser());
+        //Added Content Navdeep
+        uploadImage();
+        //End Added Content
         post.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
                 if(e != null){
                     e.printStackTrace();
                 }else {
-                    Log.d("NewPost","saved");
+
+                    Intent intent = new Intent(NewPostActivity.this, MainActivity.class);
+                    startActivity(intent);
                     finish();
                 }
             }
@@ -56,11 +87,32 @@ public class NewPostActivity extends CloseableActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        ProgressDialog progressDialog = ProgressDialog.show(NewPostActivity.this, "", "Please wait");
+        progressDialog.show();
         int id = item.getItemId();
         if(id == R.id.action_postMenu){
             savePost();
         }
         return super.onOptionsItemSelected(item);
+    }
+    private void uploadImage(){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        //compress the bitmap, save as PNG(lossless) 100 is compression for quality, 0 is compression for size
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        imageFile = new ParseFile("image.png", stream.toByteArray());
+
+        //compressBitmap(imageFile);
+        try {
+            imageFile.save();
+            post.put("Photo", imageFile);
+            post.save();
+            Toast.makeText(this, "File Uploaded", Toast.LENGTH_LONG).show();
+            //String imageUrl = imageObject.getUrl();
+            String objectId = post.getObjectId();
+            Log.d(TAG, "Object ID is " + objectId);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
